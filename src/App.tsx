a9f3b2d87ba63981bd787233bdb8319603bbcaad
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2026-06-16 17:26:09 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar";
+import Scheduling from "./pages/Scheduling";
+import PrivacySection from './pages/PrivacySection';
+import TermsOfService from './pages/TermsOfService';
+import NotFoundPage from './pages/NotFound';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StrictMode } from 'react';
+const router = createBrowserRouter([
+  {path: "/", element: <LandingPage />},
+  {path:"/dashboard", element:<Scheduling></Scheduling>},
+  {path:"/privacy", element:<PrivacySection/>},
+  {path:"/terms", element:<TermsOfService/>},
+  {path:"*", element:<NotFoundPage/>}
+])
+//Routing 
+function App() {
+  return (
+    <StrictMode>
+     
+            <RouterProvider router = {router}/>
+  
+    
+      
+    </StrictMode>
+    
+  );
+}
+
+export default App;
+
