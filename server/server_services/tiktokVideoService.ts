da23d70bff35bad0e0a3102f1206602cf0bdc533
DiamondYeah <DiamondzMartinz@gmail.com>
2026-08-08 20:07:20 +0800

    Large Tiktok videos are now split into chunks

--- server/server_services/tiktokVideoService.ts
+++ server/server_services/tiktokVideoService.ts
@@ -28,10 +28,37 @@ const TIKTOK_GETINITUPLOAD_URL = "https://open.tiktokapis.com/v2/post/publish/vi
 const TIKTOK_GETVIDEOSTATUS_URL = "https://open.tiktokapis.com/v2/post/publish/status/fetch/"
 
 
+// Determins Chunk Constraints
+const MIN_SIZE_PER_CHUNK: number = 5 * 1024 * 1024;
+const MAX_SIZE_PER_CHUNK: number = 64 * 1024 * 1024;
+
+
+// Function computes how many chunk counts are needed for the file. Needed for large file uploads
+// Each chunk is a minimum of 5 MB and a max of 64 MB (Any file larger than 64 MB needs to be split into chunks)
+// Return number of chunk counts needed
+async function calculateChunkCount(videoSize: number){
+
+    // If smaller than MIN size, just return 1 chunk
+    if(videoSize <= MIN_SIZE_PER_CHUNK)
+        return {chunk_size: videoSize, total_chunk_count: 1};
+
+
+    // If larger, then divide videoSize with the max size per chunk and perform ceiling so minimum is always 1.
+    let numberofChunks = Math.ceil(videoSize / MAX_SIZE_PER_CHUNK)
+
+    return {chunk_size: MAX_SIZE_PER_CHUNK, total_chunk_count: numberofChunks};
+
+
+}
+
+
 
 // Function initializes/prepares the video for upload to the website
 // Return JSON result of fetch including publish_id and upload_url
 export async function obtainInitialUpload(video: TikTokVideoUpload){
+
+    // Get the chunk size and chunk count by calling calculateChunkCount function
+    const  {chunk_size, total_chunk_count} = await calculateChunkCount(video.videoSize);
 
     // Post video details to TikTok for publishing and initial upload via the video parameter and API URL
     const tiktokUserInitUploadFetch = await fetch(TIKTOK_GETINITUPLOAD_URL, 
@@ -62,8 +89,8 @@ export async function obtainInitialUpload(video: TikTokVideoUpload){
 
                 source: "FILE_UPLOAD",
                 video_size: video.videoSize,
-                chunk_size:  video.videoSize,
-                total_chunk_count: 1
+                chunk_size:  chunk_size,
+                total_chunk_count: total_chunk_count
 
             }
 
@@ -101,31 +128,78 @@ export async function uploadVideo(video: Express.Multer.File, uploadURL: string)
     // Read file to buffer by finding its path location via fileSystem
     const videoBuffer = await fs.promises.readFile(video.path);
 
-    //  Performs fetch to put the video to the tiktokUser's TikTok account and return results
-    const tiktokUserUploadFetch = await fetch(uploadURL, 
-        {
-
-            method: "PUT",
-            headers:{
-
-                "Content-Type": "video/mp4",
-                "Content-Length": `${video.size}`,
-                "Content-Range": `bytes 0-${video.size - 1}/${video.size}`,
-
-            },
-            body: videoBuffer,
-
-        }
-    );
+    // Get the chunk size and chunk count by calling calculateChunkCount function
+    const  {chunk_size, total_chunk_count} = await calculateChunkCount(video.size);
 
 
-    // Check if there is error when fetching information
-    if(!tiktokUserUploadFetch.ok)
-        throw new Error("Video upload to TikTok error!");
+    if(total_chunk_count == 1){
+
+        //  Performs fetch to put the video to the tiktokUser's TikTok account and return results
+        const tiktokUserUploadFetch = await fetch(uploadURL, 
+            {
+
+                method: "PUT",
+                headers:{
+
+                    "Content-Type": "video/mp4",
+                    "Content-Length": `${video.size}`,
+                    "Content-Range": `bytes 0-${video.size - 1}/${video.size}`,
+
+                },
+                body: videoBuffer,
+
+            }
+        );
 
 
-    // Send successful JSON 
-    return tiktokUserUploadFetch
+        // Check if there is error when uploading video size
+        if(!tiktokUserUploadFetch.ok)
+            throw new Error("Video upload to TikTok error!");
+
+
+        // Send successful JSON 
+        return tiktokUserUploadFetch;
+
+
+    }
+
+
+    // Stores the res of the video uploaded in chunks
+    let lastRes;
+
+    // Loop through each chunk count, and upload each chunk 
+    for(let i = 0; i < total_chunk_count; i++){
+
+        // Computes the start and end range for the chunk, and the chunk itself
+        let startRange = i * chunk_size; // For max chunk size its i * 64MB
+        let endRange = Math.min(startRange + chunk_size, video.size) - 1;
+        const chunkBuffer = videoBuffer.subarray(startRange, endRange + 1); 
+
+        lastRes = await fetch(uploadURL, 
+                {
+
+                    method: "PUT",
+                    headers:{
+
+                        "Content-Type": "video/mp4",
+                        "Content-Length": `${chunkBuffer.length}`,
+                        "Content-Range": `bytes ${startRange}-${endRange}/${video.size}`,
+
+                    },
+
+                    body: chunkBuffer,
+
+                }
+            );
+
+        // Check if there is error when uploading chunk
+        if(!lastRes.ok)
+            throw new Error(`Video chunk ${i + 1} upload to TikTok error!`);
+
+    }
+
+
+    return lastRes;
 
 }
 
